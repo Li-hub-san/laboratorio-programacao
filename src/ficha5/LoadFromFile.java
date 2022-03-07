94e8decf445f054403970e95a3b6b316fef77bad
Li-hub-san <ligia.baptista.santos@gmail.com>
2022-03-07 13:49:31 +0000

    Added comments to file loading class.

--- src/ficha5/LoadFromFile.java
+++ src/ficha5/LoadFromFile.java
@@ -15,34 +15,46 @@ public class LoadFromFile {
     }
 
     public List<Pessoa> obterInformacaoFicheiro(String ficheiro) {
+        // para ir buscar a partir dos resources, os ficheiros têm que estar no caminho /src/main/resources
+        // pasta "ficha5" adicionada para poder diferenciar para que projectos os resources são
         String resourceName = "/ficha5/" + ficheiro;
+
+        // ler ficheiro a partir dos resources
         InputStream is = Main.class.getResourceAsStream(resourceName);
         if (is == null) {
+            // quando o ficheiro não é encontrado, envia uma excepção
             throw new NullPointerException("Cannot find resource file " + resourceName);
         }
 
+        // preparar a leitura do ficheiro
+        JSONTokener tokener = new JSONTokener(is);
+        // começar o parsing a partir de um arreio
+        JSONArray jsonArray = new JSONArray(tokener);
+
+        // lista de pessoas que vão ser criadas a partir do ficheiro
         List<Pessoa> pessoas = new ArrayList<>();
 
-        JSONTokener tokener = new JSONTokener(is);
-        JSONArray array = new JSONArray(tokener);
-
-        for (Object o : array) {
+        // iterar o arreio encontrado para cada objecto
+        for (Object object : jsonArray) {
             try {
-                JSONObject object = (JSONObject) o;
+                // converter o object num JSONObject (para poder fazer parse das propriedades)
+                JSONObject jsonObject = (JSONObject) object;
 
-                String primeiroNome = object.getString("Primeiro_nome");
-                String ultimoNome = object.getString("Ultimo_nome");
-                int idade = object.getInt("Idade");
-                double saldo = object.getDouble("Saldo");
+                // tirar os valores encontrados nas propriedades
+                String primeiroNome = jsonObject.getString("Primeiro_nome");
+                String ultimoNome = jsonObject.getString("Ultimo_nome");
+                int idade = jsonObject.getInt("Idade");
+                double saldo = jsonObject.getDouble("Saldo");
 
+                // Criar uma nova pessoa usando os valores encontrados
                 Pessoa pessoa = new Pessoa(primeiroNome, ultimoNome, idade, saldo);
+                // adicionar nova pessoa a lista para poder devolver tudo de uma vez
                 pessoas.add(pessoa);
             } catch (JSONException e) {
-                System.out.println("Erro: " + e.getMessage());
+                // apanhar possiveis erros quando estiver a fazer parse dos objectos ou propriedades
+                System.out.println("Erro de parse: " + e.getMessage());
             }
         }
-
-        System.out.println(pessoas);
 
         return pessoas;
     }
