49b6e6110f0066dc4787004a652c9676215f0899
Li-hub-san <ligia.baptista.santos@gmail.com>
2022-03-09 12:58:58 +0000

    Continued projeto extra.

---
+++ src/main/java/projeto1/menu/Menu.java
@@ -0,0 +1,10 @@
+package projeto1.menu;
+
+public class Menu {
+    public void initialMenu(){
+        System.out.println("\nBenvido à AppStore");
+
+
+        System.out.println("Obrigada pela visita. Esperamos vê-lo novamente");
+    }
+}
